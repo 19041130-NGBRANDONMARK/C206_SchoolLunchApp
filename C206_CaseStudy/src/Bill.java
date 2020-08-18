aa0a4a1326c7d3d1ac1b5cb65576857066c2dede
19016271 <19016271@DESKTOP-2MKEJKJ>
2020-08-19 03:36:23 +0800

    pushing Bill class

---
+++ C206_CaseStudy/src/Bill.java
@@ -0,0 +1,11 @@
+
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code. 
+ * I understand that copying code is considered as plagiarism.
+ *
+ *  19016271, 19 Aug 2020 3:20:11 am 
+ */
+public class Bill {
+
+}
