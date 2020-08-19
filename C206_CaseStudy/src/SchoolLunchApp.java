5b1d5ca2ce0e0c0ab5e359b8c2c8043ed08d26b0
Brandon <ngbrandonmark@hotmail.com>
2020-08-19 10:55:05 +0800

    Fixed deleteOrder

--- C206_CaseStudy/src/SchoolLunchApp.java
+++ C206_CaseStudy/src/SchoolLunchApp.java
@@ -12,17 +12,22 @@ public class SchoolLunchApp {
 	
 		
 		public static void userTypeMenu() {
-			
+			System.out.println("1. Admin login");
+			System.out.println("1. User login");
 		}
 				
 		
 		private static void optionMenu() {
 			
-
+			System.out.println("1. Display Menu Items");
+			System.out.println("2. Display Bills");
+			System.out.println("3. Display Menu");
+			System.out.println("4. Display Orders");
+			System.out.println("5. Quit");
+			
 		
 		}
 
-		
 		
 		
 		public static void addOrder(ArrayList<Order>orderArr) {
@@ -37,10 +42,10 @@ public class SchoolLunchApp {
 			 menuItem.add(new MenuItem("Set Meal", "Fishball Noodles", true, 2.50));
 			 menuItem.add(new MenuItem("Set Meal", "Chicken Rice", true, 2.00));
 			
-			 while(menuSelection.size() <=4) {
-				 
-			 }
-			 
+			
+			 Order od = new Order(studentId,orderDate,menuSelection);
+			 orderArr.add(od);
+			 System.out.println("Order added");
 			 
 		}
 		
@@ -72,7 +77,7 @@ public class SchoolLunchApp {
 							Option = true;
 							orderArr.remove(order1);
 							System.out.println("Order has been deleted successfully!");
-						} else if (option == 'n' || option == 'n') {
+						} else if (option == 'n' || option == 'N') {
 							Option = true;
 						} else {
 							System.out.println("Invalid Option!");
